118f09a3a39cbe89d45167ef6c25f47c60a56a78
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2022-08-14 14:26:23 -0500

    Init Commit

---
+++ MoneyCheat/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCheat
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(ShopData), "SetUpBase")]
+    public class StartingGoldPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ShopData __instance)
+        {
+            __instance.Gold = int.MaxValue / 2;
+        }
+    }
+}
